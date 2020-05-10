f0b70cc9c43cdc11615070533deb7eee9555154f
IlianaPinto <iliana.pinto04@gmail.com>
2020-05-10 17:32:33 -0600

    listo

--- WhatDidYouMean/src/whatdidyoumean/WhatDidYouMean.java
+++ WhatDidYouMean/src/whatdidyoumean/WhatDidYouMean.java
@@ -1,10 +1,14 @@
 package whatdidyoumean;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -17,6 +21,8 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class WhatDidYouMean {
+
+    ArrayList<String> comandos = new ArrayList();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -93,6 +99,67 @@ public class WhatDidYouMean {
         }
     }
 
+    public static void guardarArchivo(String comando) {
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try {
+            fichero = new FileWriter("./comandos.txt", true);
+            pw = new PrintWriter(fichero);
+            pw.println(comando);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fichero) {
+                    fichero.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+
+    public static ArrayList<String> leerArchivo() {
+        ArrayList<String> comandos = new ArrayList();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            archivo = new File("./comandos.txt");
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                comandos.add(linea);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return comandos;
+    }
+
+    public static boolean verificar(String token) {
+        ArrayList<String> comandos = new ArrayList();
+        comandos = leerArchivo();
+        boolean ver = false;
+        for (int i = 0; i < comandos.size(); i++) {
+            if (comandos.get(i).equals(token)) {
+                ver = true;
+            }
+        }
+        return ver;
+    }
+
     public static void tokenizer(String input) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> tokens = new ArrayList();
@@ -134,18 +201,21 @@ public class WhatDidYouMean {
                 }
                 break;
             default:
-                if (Pattern.matches("[ñ{ploikj,.][adewsqzxf]", input)) {//ls
-                    System.out.println("Did you mean ls?[y/n]");
-                    String resp = sc.next();
-                    if (resp.equalsIgnoreCase("y")) {
+                if (Pattern.matches("[ñ{ploikj,.][adewsqzxf]", tokens.get(0))) {//ls
+                    if (verificar(tokens.get(0))) {
                         ls();
                     } else {
-                        System.out.println("Ok!");
+                        System.out.println("Did you mean ls?[y/n]");
+                        String resp = sc.next();
+                        if (resp.equalsIgnoreCase("y")) {
+                            ls();
+                            guardarArchivo(tokens.get(0));
+                        } else {
+                            System.out.println("Ok!");
+                        }
                     }
-                } else if (Pattern.matches("[gfdhj][xcz][iopuy][oi+p]", input)) {//gzip
-                    System.out.println("Did you mean gzip?[y/n]");
-                    String resp = sc.next();
-                    if (resp.equalsIgnoreCase("y")) {
+                } else if (Pattern.matches("[gfdhj][xcz][iopuy][oi+p]", tokens.get(0))) {//gzip
+                    if (verificar(tokens.get(0))) {
                         for (int i = 1; i < tokens.size(); i++) {
                             guardarZIP("./" + tokens.get(i), "./" + tokens.get(i) + ".zip", tokens.get(i));
                         }
@@ -153,26 +223,57 @@ public class WhatDidYouMean {
                             System.err.println("Incomplete command gzip");
                         }
                     } else {
-                        System.out.println("Ok!");
+                        System.out.println("Did you mean gzip?[y/n]");
+                        String resp = sc.next();
+                        if (resp.equalsIgnoreCase("y")) {
+                            for (int i = 1; i < tokens.size(); i++) {
+                                guardarZIP("./" + tokens.get(i), "./" + tokens.get(i) + ".zip", tokens.get(i));
+                            }
+                            if (tokens.size() == 1) {
+                                System.err.println("Incomplete command gzip");
+                            }
+                            guardarArchivo(tokens.get(0));
+                        } else {
+                            System.out.println("Ok!");
+                        }
                     }
-                } else if (Pattern.matches("[p+oi][iopuy][nm,bv][ghjfd]", input)) {//ping
-                    System.out.println("Did you mean ping?[y/n]");
-                    String resp = sc.next();
-                    if (resp.equalsIgnoreCase("y")) {
+
+                } else if (Pattern.matches("[p+oi][iopuy][nm,bv][ghjfd]", tokens.get(0))) {//ping
+                    if (verificar(tokens.get(0))) {
                         switch (tokens.size()) {
                             case 1:
                                 System.err.println("Incomplete command ping");
+                                guardarArchivo(tokens.get(0));
                                 break;
                             case 2:
                                 ping(tokens.get(1));
+                                guardarArchivo(tokens.get(0));
                                 break;
                             default:
                                 System.err.println("More arguments than expected");
                                 break;
                         }
-
                     } else {
-                        System.out.println("Ok!");
+                        System.out.println("Did you mean ping?[y/n]");
+                        String resp = sc.next();
+                        if (resp.equalsIgnoreCase("y")) {
+                            switch (tokens.size()) {
+                                case 1:
+                                    System.err.println("Incomplete command ping");
+                                    guardarArchivo(tokens.get(0));
+                                    break;
+                                case 2:
+                                    ping(tokens.get(1));
+                                    guardarArchivo(tokens.get(0));
+                                    break;
+                                default:
+                                    System.err.println("More arguments than expected");
+                                    break;
+                            }
+
+                        } else {
+                            System.out.println("Ok!");
+                        }
                     }
                 } else {
                     System.err.println("Sorry, \"" + tokens.get(0) + "\" is not recognized 😕");
